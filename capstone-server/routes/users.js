4badc13fc3b8a49171015aadb363ed1354fe40ba
Randi Reyna <77865204+randi09@users.noreply.github.com>
2021-06-17 10:12:46 -0500

    deleted duplicates

--- capstone-server/routes/users.js
+++ capstone-server/routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
-
+var express = require("express");
+var app = express.Router();
+const models = require("../models");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+app.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-module.exports = router;
+// Creating a User
+app.post(
+  "/users",
+  async (req, res) => {
+    const { firstName, lastName, email, password } = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    //sending back user's ID in the response.
+    res.json({
+      id: newUser.id,
+    });
+  },
+
+  app.get("/users", async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+  })
+);
+module.exports = app;
