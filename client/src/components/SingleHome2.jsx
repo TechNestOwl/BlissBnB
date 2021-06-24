f4c97696678b2f0c8bc666569d0a283d0b6ada09
Gill Guimaraes <63368519+TechNestOwl@users.noreply.github.com>
2021-06-24 10:15:24 -0400

    changed about home info

--- client/src/components/SingleHome2.jsx
+++ client/src/components/SingleHome2.jsx
@@ -26,127 +26,141 @@ export default function SingleHome2() {
 
     return (
         <div>
-            <Container>
+            <Container className="singleHomeParentContainer">
                 {/* Stack the columns on mobile by making one full-width and the other half-width */}
-                <div className="singleHomePhotoContainer">
-                    <Carousel>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={beachHouseInterior1}
-                                alt="First slide"
-                            />
-                            <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={beachHouseInterior2}
-                                alt="Second slide"
-                            />
+                <Col sm={10}>
+                    <div className="singleHomePageCenter">
 
-                            <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src={coastalbeachhouse}
-                                alt="Third slide"
-                            />
+                        <div className="singleHomePhotoContainer">
+                            <Carousel>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={coastalbeachhouse}
+                                        alt="First slide"
+                                    />
+                                    <Carousel.Caption>
+                                        <h3>Welcome Home</h3>
+                                        {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={beachHouseInterior1}
+                                        alt="Second slide"
+                                    />
 
-                            <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
+                                    <Carousel.Caption>
+                                        <h3>Living Room Views</h3>
+                                        {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={beachHouseInterior2}
+                                        alt="Third slide"
+                                    />
 
-                </div>
-
-
-                {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
-                <Row>
-                    <Col xs={6} md={4}>
-                        <h4>About this space:</h4>
-                        <p>Single family home with with beach front access and loads of lorem ipsum dolor sit amet,
-                            consectetur adipiscing elit, sed do eiusmod tempor
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-                    </Col>
-                    <Col xs={6} md={4}>
-                        <h4>About the home</h4>
-                        <p>6 Guests</p>
-                        <p>3 Bedroom</p>
-                        <p>2 Bath</p>
-
-                        <div className="ammenityListContainer">
-                            <h4>Amenities</h4>
-                            <ListGroup variant="flush">
-                                <ListGroup.Item>Hottub</ListGroup.Item>
-                                <ListGroup.Item>Firepit</ListGroup.Item>
-                                <ListGroup.Item>Washer/Dryer</ListGroup.Item>
-                                <ListGroup.Item>Full stone fireplace</ListGroup.Item>
-                                <ListGroup.Item>Full kitchen</ListGroup.Item>
-                                <ListGroup.Item>Beach Access</ListGroup.Item>
-                                <ListGroup.Item>Charcoal BBQ grill</ListGroup.Item>
-                            </ListGroup>
+                                    <Carousel.Caption>
+                                        <h3>Gourmet Kitchen</h3>
+                                        {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+                                    </Carousel.Caption>
+                                </Carousel.Item>
+                            </Carousel>
 
                         </div>
-                    </Col>
-                    <Col xs={6} md={4}>
 
-                        <Image src={hostHeadshot} roundedCircle />
-                        <h4>Host:</h4>
-                        <h5>Gabriella Parks</h5>
-                    </Col>
-                </Row>
 
-                {/* Columns are always 50% wide, on mobile and desktop */}
-                <Row>
-                    <Col xs={6}>
-                        <div className="reservationCard">
-                            <div className="reservationPrice">
-                                <span className="slashedPriced">$189 </span>
-                                <span> $133 / night</span>
-                            </div>
-                            <div className="reservationCal">
-                                <div>
-                                    <Calendar
-                                        // value={calDate}
-                                        onChange={e => setCalDate(e.target.value)} />
-                                </div>
-                                <form onSubmit={handleSubmit} className="reservatinForm">
-                                    <p>Guests:</p>
-                                    <button>-</button>
-                                    <span >0</span>
-                                    <button>+</button>
-                                    <button className="submitBtn" type="submit">Add Date</button>
-                                </form>
-
-                            </div>
-
-                        </div>
-                    </Col>
-                    <Col xs={6}>
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={logo} />
-                            <Card.Body>
-                                <Card.Title>Coastal Beach Home</Card.Title>
-                                <Card.Text>
-                                    Single family home with beach from access and lots of lorem ipsum dolor sit amet,
+                        {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+                        <Row>
+                            <Col xs={6} md={4}>
+                                <h4>About this space:</h4>
+                                <p>Single family home with with beach front access and loads of lorem ipsum dolor sit amet,
                                     consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore magna aliqua.
-                                </Card.Text>
-                                <Button variant="primary">Book Home</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
+                                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+                            </Col>
+                            <Col xs={6} md={4}>
+                                {/* <h4>About the home</h4>
+                                <p>6 Guests</p>
+                                <p>3 Bedroom</p>
+                                <p>2 Bath</p> */}
+                                <div className="ammenityListContainer">
+                                    <h4>Amenities</h4>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item>Hottub</ListGroup.Item>
+                                        <ListGroup.Item>Firepit</ListGroup.Item>
+                                        <ListGroup.Item>Washer/Dryer</ListGroup.Item>
+                                        <ListGroup.Item>Full stone fireplace</ListGroup.Item>
+                                        <ListGroup.Item>Full kitchen</ListGroup.Item>
+                                        <ListGroup.Item>Beach Access</ListGroup.Item>
+                                        <ListGroup.Item>Charcoal BBQ grill</ListGroup.Item>
+                                    </ListGroup>
+
+                                </div>
+                            </Col>
+
+                        </Row>
+
+                        {/* Columns are always 50% wide, on mobile and desktop */}
+                        <Row>
+                            <Col xs={6}>
+                                <div className="reservationCard">
+                                    <div className="reservationPrice">
+                                        <span className="slashedPriced">$189 </span>
+                                        <span> $133 / night</span>
+                                    </div>
+                                    <div className="reservationCal">
+                                        <div>
+                                            <Calendar
+                                                // value={calDate}
+                                                onChange={e => setCalDate(e.target.value)} />
+                                        </div>
+                                        <form onSubmit={handleSubmit} className="reservatinForm">
+                                            <p>Guests:</p>
+                                            <button>-</button>
+                                            <span >0</span>
+                                            <button>+</button>
+                                            <button className="submitBtn" type="submit">Add Date</button>
+                                        </form>
+
+                                    </div>
+
+                                </div>
+                            </Col>
+                            <Col xs={6}>
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src={logo} />
+                                    <Card.Body>
+                                        <Card.Title>Coastal Beach Home</Card.Title>
+                                        <Card.Text>
+                                            Single family home with beach from access and lots of lorem ipsum dolor sit amet,
+                                            consectetur adipiscing elit, sed do eiusmod tempor
+                                            incididunt ut labore et dolore magna aliqua.
+                                        </Card.Text>
+                                        <Button variant="primary">Book Home</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
+                <Col sm={2}>
+                    <div className="singleHomePageRight">
+                        <Col xs={6} md={4}>
+                            <Image src={hostHeadshot} roundedCircle />
+                            <h4>Host:</h4>
+                            <h5>Gabriella Parks</h5>
+                            <br></br>
+                            <hr></hr>
+                            <h4>About the home</h4>
+                            <p>6 Guests</p>
+                            <p>3 Bedroom</p>
+                            <p>2 Bath</p>
+                        </Col>
+                    </div>
+                </Col>
             </Container>
         </div>
     )
